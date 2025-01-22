fa2208086c43bda056a33dab3e2f272e80513914
Cyril Zakka <1841186+cyrilzakka@users.noreply.github.com>
2025-01-21 21:45:37 -0800

    Content fades correctly on scroll

--- HuggingChat-Mac/DetailView/ChatView.swift
+++ HuggingChat-Mac/DetailView/ChatView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ChatView: View {
     
-    var isPipMode: Bool = false
+    var isPipMode: Bool = true
     var onPipToggle: () -> Void
     @EnvironmentObject private var appDelegate: AppDelegate
     @Environment(\.colorScheme) var colorScheme
@@ -17,94 +17,126 @@ struct ChatView: View {
     @State private var showingPopover = false
     @State private var showPipToolbar: Bool = false
     
+    private var backgroundMaterial: some View {
+            ZStack {
+//                if !isPipMode {
+                    Rectangle.semiOpaqueWindow()
+//                }
+                Rectangle().fill(.regularMaterial)
+            }
+        }
+    
     var body: some View {
         ZStack {
-            if !isPipMode {
-                Rectangle.semiOpaqueWindow()
-            }
-            Rectangle().fill(.regularMaterial)
-            
-                
-                
-                    
-                    ScrollView(.vertical) {
-                        
+            backgroundMaterial
+
+            ScrollView(.vertical) {
+#if DEBUG
+                LazyVStack {
+                    ForEach(1...100, id: \.self) { value in
+                        Text("Row \(value)")
                     }
-                    .safeAreaInset(edge: .top, content: {
-                        if isPipMode && showPipToolbar {
-                            HStack {
-                                Button(action: {
-                                    onPipToggle()
-                                }, label: {
-                                    Image(systemName: "xmark.circle.fill")
-                                })
-                                .buttonStyle(.plain)
-                                
-                                Spacer()
-          
-                                Button(action: {
-                                    // Exit pip mode
-                                }, label: {
-                                    Image(systemName: "pip.exit")
-                                })
-                                .buttonStyle(.accessoryBar)
-                                
-                                
-                                Button(action: {
-                                    // new conversation
-                                }, label: {
-                                    Image(systemName: "square.and.pencil")
-                                })
-                                .buttonStyle(.accessoryBar)
-                                
-                            }
-                            
-                            .padding(.horizontal)
-                            .padding(.top, 10)
-//                            .background(.regularMaterial)
-                        }
-                    })
-                    .safeAreaInset(edge: .bottom, content: {
-                        InputView()
-                            .padding([.horizontal, .bottom])
-                    })
-                    .toolbar {
-                        ToolbarItemGroup(placement: .navigation) {
-                            Button(action: {
-                                showingPopover = true
-                            }, label: {
-                                HStack(spacing: 5) {
-                                    Text("DeepSeek-R1")
-                                        .foregroundStyle(colorScheme == .dark ? .white : .black)
-                                        .fontWeight(.medium)
-                                        .font(.title3)
-                                    Image(systemName: "chevron.right")
-                                        .imageScale(.small)
-                                }
-                            })
-                            .buttonStyle(.accessoryBar)
-                            .popover(isPresented: $showingPopover, arrowEdge: .bottom) {
-                                Text("Your content here")
-                                    .font(.headline)
-                                    .padding()
-                            }
-                            
-                        }
-                        
-                        ToolbarItemGroup(placement: .primaryAction) {
-                            Button(action: {
-                                // Share conversation
-                            }, label: {
-                                Image(systemName: "square.and.arrow.up")
-                            })
+                }
+#endif
+            }
+            .contentMargins(.bottom, -20, for: .scrollContent)
+            .contentMargins(.top, 20, for: .scrollContent)
+            .overlay {
+                VStack {
+                    if isPipMode && showPipToolbar {
+                        HStack {
                             Button(action: {
                                 onPipToggle()
                             }, label: {
-                                Image(systemName: "pip")
+                                Image(systemName: "xmark.circle.fill")
                             })
+                            .buttonStyle(.plain)
+                            
+                            Spacer()
+                            
+                            Button(action: {
+                                // Exit pip mode
+                            }, label: {
+                                Image(systemName: "pip.exit")
+                            })
+                            .buttonStyle(.accessoryBar)
+                            
+                            
+                            Button(action: {
+                                // new conversation
+                            }, label: {
+                                Image(systemName: "square.and.pencil")
+                            })
+                            .buttonStyle(.accessoryBar)
+                            
                         }
+                        
+                        .padding(.horizontal)
+                        .padding(.top, 10)
+                        .background {
+                            backgroundMaterial
+                        }
+                        
+                        Spacer()
+                            .allowsHitTesting(false)
+                        
                     }
+                }
                 
+                
+            }
+            .safeAreaInset(edge: .bottom, content: {
+                InputView()
+                    .padding([.horizontal, .bottom])
+                    .padding(.top, 50)
+                    
+                    .background {
+                        backgroundMaterial
+                            .mask(LinearGradient(gradient: Gradient(stops: [
+                                .init(color: .black, location: 0),
+                                .init(color: .black, location: 0.7),
+                                .init(color: .clear, location: 1)
+                            ]), startPoint: .bottom, endPoint: .top))
+                            
+                    }
+            })
+            .toolbar {
+                ToolbarItemGroup(placement: .navigation) {
+                    Button(action: {
+                        showingPopover = true
+                    }, label: {
+                        HStack(spacing: 5) {
+                            Text("DeepSeek-R1")
+                                .foregroundStyle(colorScheme == .dark ? .white : .black)
+                                .fontWeight(.medium)
+                                .font(.title3)
+                            Image(systemName: "chevron.right")
+                                .imageScale(.small)
+                        }
+                    })
+                    .buttonStyle(.accessoryBar)
+                    .popover(isPresented: $showingPopover, arrowEdge: .bottom) {
+                        Text("Your content here")
+                            .font(.headline)
+                            .padding()
+                    }
+                    
+                }
+                
+                ToolbarItemGroup(placement: .primaryAction) {
+                    Button(action: {
+                        // Share conversation
+                    }, label: {
+                        Image(systemName: "square.and.arrow.up")
+                    })
+                    Button(action: {
+                        onPipToggle()
+                    }, label: {
+                        Image(systemName: "pip")
+                    })
+                }
+            }
+            
             
         }
         .onHover { over in
@@ -123,6 +155,8 @@ struct ChatView: View {
 }
 
 #Preview(traits: .sizeThatFitsLayout) {
-    ContentView()
+    ChatView(isPipMode: true) {
+        
+    }
         .environmentObject(AppDelegate())
 }
--- HuggingChat-Mac/DetailView/InputView.swift
+++ HuggingChat-Mac/DetailView/InputView.swift
@@ -38,6 +38,7 @@ struct InputView: View {
             } else {
                 RoundedRectangle(cornerRadius: cornerRadius)
                     .stroke(.gray.opacity(0.5), lineWidth: 1)
+                    .fill(.ultraThickMaterial)
                     .fill(.quinary)
             }
         }
